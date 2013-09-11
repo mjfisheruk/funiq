1d50b875dd960a2d97a92dead4b5900fccb1c0b6
Matt Fisher <general@mjfisher.net>
2013-09-11 19:44:54 +0100

    Adds basic loop structure and matching

--- src/funiq.cpp
+++ src/funiq.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <map>
 #include <iostream>
 #include <fstream>
 #include <tclap/CmdLine.h>
@@ -32,13 +33,52 @@ void setup(int argc, char** argv, vector<string>& lines) {
 
 int main(int argc, char* argv[]) {
 
+	typedef map< string, vector<string>* > StringListMap;
+
 	try {
 
+		StringListMap matches;
 		vector<string> lines(0);
 		setup(argc, argv, lines);
+		StringListMap::iterator matchIt;
+		vector<string>::iterator matchItemIt;
 
-		for(vector<string>::iterator it = lines.begin(); it != lines.end(); ++it) {
-		    cout << *it << endl;
+		//For every line in input, compare with every key in the hashmap
+		vector<string>::iterator inputIt;
+		for(inputIt = lines.begin(); inputIt != lines.end(); ++inputIt) {
+		    string line = *inputIt;
+		    bool matchFound = false;
+		    string matchKey;
+		    for(matchIt = matches.begin(); matchIt != matches.end(); ++matchIt) {
+		    	pair<string, vector<string>* > matchPair = *matchIt;
+		    	vector<string> matchList = *matchPair.second;
+		    	for(matchItemIt = matchList.begin(); matchItemIt != matchList.end(); ++matchItemIt) {
+		    		string matchItem = *matchItemIt;
+		    		if(matchItem == line) {
+		    			matchFound = true;
+		    			matchKey = matchPair.first;
+		    			continue;
+		    		}
+		    	}
+		    }
+		    if(matchFound) {
+    			vector<string>* v = matches[matchKey];
+    			v->push_back(line);
+		    } else {	
+		    	vector<string>* v = new vector<string>(0);
+		    	v->push_back(line);
+		    	matches[line] = v;
+		    }
+		}
+
+		for(matchIt = matches.begin(); matchIt != matches.end(); ++matchIt) {
+			pair<string, vector<string>* > matchPair = *matchIt;
+			vector<string> v = *matchPair.second;
+			for(matchItemIt = v.begin(); matchItemIt != v.end(); ++matchItemIt) {
+		    	string matchItem = *matchItemIt;
+		    	cout << matchItem << " ";
+		    }
+		    cout << endl;
 		}
 
 	} catch (TCLAP::ArgException &e) {

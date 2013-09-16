05695955f41d2c0ede8b0d547ebbc267c7185ba6
Matt Fisher <general@mjfisher.net>
2013-09-16 20:19:32 +0100

    Fixed formatting

--- src/funiq.cpp
+++ src/funiq.cpp
@@ -53,26 +53,26 @@ void readLines(const string& filename, StringList& lines) {
 }
 
 unsigned int levenshteinDistance(const string& s1, const string& s2) {
-    
-    unsigned int len1 = s1.size();
-    unsigned int len2 = s2.size();
-    vector<unsigned int> col(len2+1);
-    vector<unsigned int> prevCol(len2+1);
+	
+	unsigned int len1 = s1.size();
+	unsigned int len2 = s2.size();
+	vector<unsigned int> col(len2+1);
+	vector<unsigned int> prevCol(len2+1);
 
-    for (unsigned int i = 0; i < prevCol.size(); i++) {
+	for (unsigned int i = 0; i < prevCol.size(); i++) {
 		prevCol[i] = i;
-    }
-    for (unsigned int i = 0; i < len1; i++) {
+	}
+	for (unsigned int i = 0; i < len1; i++) {
 		col[0] = i+1;
 		for (unsigned int j = 0; j < len2; j++) {
 			col[j+1] = min(
 				min( 1 + col[j], 1 + prevCol[1 + j]),
-		        prevCol[j] + (s1[i]==s2[j] ? 0 : 1)
-		    );
+				prevCol[j] + (s1[i]==s2[j] ? 0 : 1)
+			);
 		}
 		col.swap(prevCol);
-    }
-    return prevCol[len2];
+	}
+	return prevCol[len2];
 }
 
 void lowercase(string& s) {
@@ -85,32 +85,32 @@ void buildMap(StringList& lines, StringListMap& matchMap, const FuniqSettings& s
 	StringListMap::iterator matchMapIt;
 
 	for(linesIt = lines.begin(); linesIt != lines.end(); ++linesIt) {
-	    bool matchFound = false;
-	    string originalLine = *linesIt;
+		bool matchFound = false;
+		string originalLine = *linesIt;
 
-	    string line = originalLine;
-	    if(settings.caseInsensitive) lowercase(line);
+		string line = originalLine;
+		if(settings.caseInsensitive) lowercase(line);
 
-	    for(matchMapIt = matchMap.begin(); matchMapIt != matchMap.end(); ++matchMapIt) {
-	    	
-	    	StringListPair matchPair = *matchMapIt;	
-	    	string key = matchPair.first;
-	    	if(settings.caseInsensitive) lowercase(key);
-	    	StringList* matchList = matchPair.second;	
-    		
-    		if(levenshteinDistance(line, key) <= settings.maxEditDistance) {
-    			matchFound = true;
-    			key = matchPair.first;
-    			matchList->push_back(originalLine);
-    			continue;
-    		}
-	    }
+		for(matchMapIt = matchMap.begin(); matchMapIt != matchMap.end(); ++matchMapIt) {
+			
+			StringListPair matchPair = *matchMapIt;	
+			string key = matchPair.first;
+			if(settings.caseInsensitive) lowercase(key);
+			StringList* matchList = matchPair.second;	
+			
+			if(levenshteinDistance(line, key) <= settings.maxEditDistance) {
+				matchFound = true;
+				key = matchPair.first;
+				matchList->push_back(originalLine);
+				continue;
+			}
+		}
 
-	    if(!matchFound) {
-	    	StringList* matchList = new StringList(0);
-	    	matchList->push_back(originalLine);
-	    	matchMap[line] = matchList;
-	    }
+		if(!matchFound) {
+			StringList* matchList = new StringList(0);
+			matchList->push_back(originalLine);
+			matchMap[line] = matchList;
+		}
 	}
 
 }
@@ -124,10 +124,10 @@ void displayResults(StringListMap& matchMap) {
 		StringListPair matchPair = *matchIt;
 		StringList v = *matchPair.second;
 		for(matchItemIt = v.begin(); matchItemIt != v.end(); ++matchItemIt) {
-	    	string matchItem = *matchItemIt;
-	    	cout << matchItem << "\t";
-	    }
-	    cout << endl;
+			string matchItem = *matchItemIt;
+			cout << matchItem << "\t";
+		}
+		cout << endl;
 	}
 }
 
